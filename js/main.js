fc484bdc0b77be34f87ef67664adb98c1d7ac129
Mohammad Sagor <68495729+sagorbbr1@users.noreply.github.com>
2022-02-12 23:39:03 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,42 @@
+window.onload = function () {
+    main();
+
+}
+
+function main () {
+    const btn = document.querySelector('#btn');
+    const root = document.querySelector('#root');
+
+    
+    btn.addEventListener('click', function () {
+        const bgColor = createRGB();
+        
+
+        root.style.backgroundColor = bgColor;
+    
+    
+    })
+
+
+
+}
+
+function createRGB() {
+    const red = Math.floor(Math.random() * 255) + 1;
+    const green = Math.floor(Math.random() * 255) + 1;
+    const blue = Math.floor(Math.random() * 255) + 1;
+
+    return `rgb(${red}, ${green}, ${blue})`
+}
+
+
+// const btn = document.querySelector('#btn');
+
+// btn.addEventListener('click', function (e) {
+    
+//     const randColor = Math.floor(Math.random() * 255) + 1;
+
+//     const body = document.documentElement.lastChild;
+
+//     body.style.backgroundColor = `#${randColor}${randColor}`;
+// })
